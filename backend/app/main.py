d76cb77de74b42cb6cb7e32cbdb16c292f84d017
ZiaMalik-wq <ziam47964@gmail.com>
2025-11-29 12:12:31 +0500

    Fix: Implemented Lazy Loading to  prevent deployment timeout again

--- backend/app/main.py
+++ backend/app/main.py
@@ -64,3 +64,10 @@ def read_root():
 @app.get("/health", tags=["Status"])
 def health_check():
     return {"status": "ok"}
+
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))  # fallback for local dev
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
