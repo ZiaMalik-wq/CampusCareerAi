83e27b3b537eb6b16c788fec50e645044466779e
ZiaMalik-wq <ziam47964@gmail.com>
2025-11-30 14:07:01 +0500

    Fix: Job Details Page

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://campuscareerai.onrender.com'; 
-const BASE_URL = 'http://127.0.0.1:8000'; 
+const BASE_URL = 'https://campuscareerai.onrender.com';
 
 const api = axios.create({
   baseURL: BASE_URL,
